3525f4f30d9ce97a5f608f092b2639c89bf5ae2f
szelongq <72333104+szelongq@users.noreply.github.com>
2022-08-06 15:08:13 +0800

    Add pause game functionality
    Add countdown before game starts

---
+++ Typist/Assets/Scripts/CountdownManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownManager : MonoBehaviour
+{
+
+    [SerializeField]
+    TMP_Text countdownText;
+
+    [SerializeField]
+    GameManager gameManager;
+
+    bool isCountingDown;
+    float countdownTime;
+    const float COUNTDOWN_TIME = 3f;
+
+    void Awake()
+    {
+        isCountingDown = false;
+        countdownTime = 0f;
+    }
+
+    void Start()
+    {
+        StartCountdown();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isCountingDown)
+        {
+            countdownTime -= Time.deltaTime;
+            if (countdownTime <= 0f)
+            {
+                StopCountdown();
+            }
+
+            DisplayCountdown(countdownTime);
+        }
+
+    }
+
+    public void StartCountdown()
+    {
+        isCountingDown = true;
+        countdownTime = COUNTDOWN_TIME;
+    }
+
+    public void StopCountdown() {
+        isCountingDown = false;
+        countdownTime = 0f;
+        gameManager.StartGame();
+        gameObject.SetActive(false);
+    }
+
+    void DisplayCountdown(float timeLeft)
+    {
+        countdownText.text = string.Format("{0}", Mathf.CeilToInt(timeLeft));
+    }
+}
